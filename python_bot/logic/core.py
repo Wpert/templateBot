e438be10d5a8f2e995c8df281ebe20c4bc923709
Vpert <amorikov95@gmail.com>
2023-09-06 17:02:36 +0000

    Hello!

--- python_bot/logic/core.py
+++ python_bot/logic/core.py
@@ -6,10 +6,10 @@
 import typing
 from typing import Dict, Any
 
-TOKEN: str = "6148619132:AAFcPv-zte6-H1KYL_LRsbY-bUuLFs9_srA"
-loggerChat_id: int = -986688477
-qnaChat_id: int = -882729066
-bot_id: int = 6148619132
+TOKEN: str = "token"
+loggerChat_id: int = 0
+qnaChat_id: int = 0
+bot_id: int = 0
 
 import logging
 from aiogram import Bot, Dispatcher, types
@@ -23,7 +23,7 @@ dp = Dispatcher()
 #     id2 : {data about user_id2},
 #     ...
 #  }
-userDataBase: Dict[int, Dict[str, Any]] = {1413950580 : {'username' : 'Vpert', 'status' : 4}}
+# userDataBase: Dict[int, Dict[str, Any]] = {1413950580 : {'username' : 'Vpert', 'status' : 4}}
 
 # {
 #     id1 : "Well, answer to your question...",
